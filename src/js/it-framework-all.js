9c15c64f6c794e0f9165580823bbd490fac1b45e
Teguh Muhammad Zundi <teguhzundi@gmail.com>
2017-09-12 14:01:00 +0700

    Select

--- src/js/it-framework-all.js
+++ src/js/it-framework-all.js
@@ -1684,9 +1684,6 @@ IT.Select = class extends IT.FormItem {
 				type	: 'array',
 				data	: null,
 			},
-			selectize: {
-				allowEmptyOption: true
-			}
 		}, settings);
 		
 		me.id = me.settings.id || IT.Utils.id();
@@ -1694,6 +1691,7 @@ IT.Select = class extends IT.FormItem {
 		me.input = $('<select />', {
 			id: me.id,
 			name: me.id,
+			class: 'it-edit-input',
 			attr: {
 				disabled: me.settings.disabled,
 			},
@@ -1701,7 +1699,6 @@ IT.Select = class extends IT.FormItem {
 		});
 		me.content = $('<div />', { class: 'it-edit' });
 		me.content.append(me.input);
-		me.input.selectize(me.settings.selectize);
 
 		if(me.settings.width) {
 			me.content.css({
@@ -1714,17 +1711,16 @@ IT.Select = class extends IT.FormItem {
 			"onChange"
 		]);
 		
-		me.getSelect().on("change",function(){
+		me.input.on("change",function(){
 			me.doEvent("onChange",[me,me.val()]);
 		});
 
 		// If has value of empty text
 		if(me.settings.emptyText && !me.settings.autoLoad) {
-			me.getSelect().addOption({
-				value: '',
+			me.input.append($('<option/>', {
+				val: '',
 				text: me.settings.emptyText
-			});
-			me.getSelect().setValue('');
+			}));
 		}
 
 		//setting store
@@ -1738,13 +1734,15 @@ IT.Select = class extends IT.FormItem {
 				me.readyState = false;
 			},
 			afterLoad:function(ev,cls,data){
-				data.forEach((obj)=>
-					me.getSelect().addOption({ 
-						value: obj.rawData.key, 
-						text: obj.rawData.value, 
-						params: typeof obj.rawData.params !== 'undefined' ? JSON.stringify(obj.rawData.params) : ''
-					})
-				);
+				data.forEach((obj)=> {
+					me.input.append($('<option/>', {
+						val: obj.rawData.key,
+						text: obj.rawData.value,
+						attr: {
+							'data-params' : (typeof obj.rawData.params !== 'undefined' ? JSON.stringify(obj.rawData.params) : '')
+						}
+					}));
+				});
 				me.readyState = true;
 			}
 		}); 
@@ -1765,24 +1763,19 @@ IT.Select = class extends IT.FormItem {
 	getDataStore() {
 		let me = this;
 		let ds = me.settings.store; 
-		let selectize = me.getSelect();
 
-		//Empty Option
-		selectize.clearOptions();
+		me.input.empty();
 
 		// If has value of empty text
 		if(me.settings.emptyText) {
-			selectize.addOption({
-				value: '',
+			me.input.append($('<option/>', {
+				val: '',
 				text: me.settings.emptyText
-			});
-			selectize.setValue('');
+			}));
 		}
 		me.store.load();
 	}
-	getSelect() {
-		return this.input[0].selectize;
-	}
+	
 	/**
 	 * Override
 	 * @param  {Optional} value Value to setter
@@ -1790,8 +1783,8 @@ IT.Select = class extends IT.FormItem {
 	 */
 	val(value) {
 		if (typeof value === "undefined")
-			return this.getSelect().getValue();
-		else return this.getSelect().setValue(value);
+			return this.input.val();
+		else return this.input.val(value);
 	}
 }
 
--- src/js/lib/Select.js
+++ src/js/lib/Select.js
@@ -18,9 +18,6 @@ IT.Select = class extends IT.FormItem {
 				type	: 'array',
 				data	: null,
 			},
-			selectize: {
-				allowEmptyOption: true
-			}
 		}, settings);
 		
 		me.id = me.settings.id || IT.Utils.id();
@@ -28,6 +25,7 @@ IT.Select = class extends IT.FormItem {
 		me.input = $('<select />', {
 			id: me.id,
 			name: me.id,
+			class: 'it-edit-input',
 			attr: {
 				disabled: me.settings.disabled,
 			},
@@ -35,7 +33,6 @@ IT.Select = class extends IT.FormItem {
 		});
 		me.content = $('<div />', { class: 'it-edit' });
 		me.content.append(me.input);
-		me.input.selectize(me.settings.selectize);
 
 		if(me.settings.width) {
 			me.content.css({
@@ -48,17 +45,16 @@ IT.Select = class extends IT.FormItem {
 			"onChange"
 		]);
 		
-		me.getSelect().on("change",function(){
+		me.input.on("change",function(){
 			me.doEvent("onChange",[me,me.val()]);
 		});
 
 		// If has value of empty text
 		if(me.settings.emptyText && !me.settings.autoLoad) {
-			me.getSelect().addOption({
-				value: '',
+			me.input.append($('<option/>', {
+				val: '',
 				text: me.settings.emptyText
-			});
-			me.getSelect().setValue('');
+			}));
 		}
 
 		//setting store
@@ -72,13 +68,15 @@ IT.Select = class extends IT.FormItem {
 				me.readyState = false;
 			},
 			afterLoad:function(ev,cls,data){
-				data.forEach((obj)=>
-					me.getSelect().addOption({ 
-						value: obj.rawData.key, 
-						text: obj.rawData.value, 
-						params: typeof obj.rawData.params !== 'undefined' ? JSON.stringify(obj.rawData.params) : ''
-					})
-				);
+				data.forEach((obj)=> {
+					me.input.append($('<option/>', {
+						val: obj.rawData.key,
+						text: obj.rawData.value,
+						attr: {
+							'data-params' : (typeof obj.rawData.params !== 'undefined' ? JSON.stringify(obj.rawData.params) : '')
+						}
+					}));
+				});
 				me.readyState = true;
 			}
 		}); 
@@ -99,24 +97,19 @@ IT.Select = class extends IT.FormItem {
 	getDataStore() {
 		let me = this;
 		let ds = me.settings.store; 
-		let selectize = me.getSelect();
 
-		//Empty Option
-		selectize.clearOptions();
+		me.input.empty();
 
 		// If has value of empty text
 		if(me.settings.emptyText) {
-			selectize.addOption({
-				value: '',
+			me.input.append($('<option/>', {
+				val: '',
 				text: me.settings.emptyText
-			});
-			selectize.setValue('');
+			}));
 		}
 		me.store.load();
 	}
-	getSelect() {
-		return this.input[0].selectize;
-	}
+	
 	/**
 	 * Override
 	 * @param  {Optional} value Value to setter
@@ -124,7 +117,7 @@ IT.Select = class extends IT.FormItem {
 	 */
 	val(value) {
 		if (typeof value === "undefined")
-			return this.getSelect().getValue();
-		else return this.getSelect().setValue(value);
+			return this.input.val();
+		else return this.input.val(value);
 	}
 }
